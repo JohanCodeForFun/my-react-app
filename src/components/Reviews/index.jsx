d273c53908bc2aa64e12a4f8bfed4ade86483780
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-09-30 21:02:15 +0200

    clean up from demo

--- src/components/Reviews/index.jsx
+++ src/components/Reviews/index.jsx
@@ -27,7 +27,6 @@ function Reviews() {
       try {
         const response = await fetch(`${import.meta.env.VITE_API_URL}`);
         const data = await response.json();
-        console.log("fetch", data)
         setReviews(data);
       } catch (error) {
         console.error('Error fetching reviews:', error);
@@ -41,7 +40,6 @@ function Reviews() {
     <article className='review'>
       <h3>Vad andra säger om oss</h3>
       <List reviews={reviews} />
-      <a href="/tyck-till">TEST till lämna omdöme</a>
       <Link className='button button--review' to="tyck-till">Lämna omdöme</Link>
     </article>
   );
